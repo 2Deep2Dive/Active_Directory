f80e0848aa78bc6a512db63cf3235cc888a74096
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2021-07-29 16:07:07 +0200

    edited name to module build file

--- Build.ps1
+++ Build.ps1
@@ -6,5 +6,5 @@ if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Fo
 if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -Force -Verbose}
 if (!(Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Force -Verbose}
 if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Force -Verbose }
-Invoke-psake -buildFile ".\*\ModuleName.build.ps1" -taskList $Task -Verbose:$VerbosePreference
+Invoke-psake -buildFile ".\*\SyncADContacts.build.ps1" -taskList $Task -Verbose:$VerbosePreference
 if ($psake.build_success -eq $false) {exit 1 } else { exit 0 }
