6099b3a9d3a46ca92ca6f8c213be165800fe5e4a
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2021-07-30 09:46:33 +0200

    added check for test files

--- AppVeyor/AppVeyorTest.ps1
+++ AppVeyor/AppVeyorTest.ps1
@@ -24,15 +24,15 @@ Write-Host "Current working directory: $pwd"
 #---------------------------------#
 $resultsFile = '.\TestsResults.xml'
 $testFiles   = Get-ChildItem "$pwd\tests" | Where-Object {$_.FullName -match 'Tests.ps1$'} | Select-Object -ExpandProperty FullName
-$results     = Invoke-Pester -Script $testFiles -OutputFormat NUnitXml -OutputFile $resultsFile -PassThru
-
-Write-Host 'Uploading results'
-try {
-  (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $resultsFile))
-} catch {
-  throw "Upload failed."
+if(!($null = $testFiles)){
+  $results     = Invoke-Pester -Script $testFiles -OutputFormat NUnitXml -OutputFile $resultsFile -PassThru
+  Write-Host 'Uploading results'
+  try {
+    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $resultsFile))
+  } catch {
+    throw "Upload failed."
+  }
 }
-
 #---------------------------------#
 # Validate                        #
 #---------------------------------#
