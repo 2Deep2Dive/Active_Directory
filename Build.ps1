922dc1bc5ffdf0dcdeaee5a032c2a954e43eeee5
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2021-08-04 12:13:39 +0200

    removed update command for PSScriptAnalyzer

--- Build.ps1
+++ Build.ps1
@@ -5,7 +5,7 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force;
 if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Force} else{update-Module -Name pester -Force};
 if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -MinimumVersion 4.8.0 -Force} else{update-Module -Name psake -Force}
 if (!(Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Force -Verbose}        else{update-Module -Name PSDeploy -Force}
-if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Force} else{Update-Module -Name PSScriptAnalyzer -Force}
+if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Force}
 
 switch ($Task) {
     'Analyze' {
