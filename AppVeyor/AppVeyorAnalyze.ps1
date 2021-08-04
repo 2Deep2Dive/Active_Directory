454b2946edb6a72f8cf664c6f548f0884ab313f2
Eliwa Mohamed <mohamed.eliwa@profidata.com>
2021-08-04 12:10:33 +0200

    new analyze task

--- AppVeyor/AppVeyorAnalyze.ps1
+++ AppVeyor/AppVeyorAnalyze.ps1
@@ -1,21 +1,6 @@
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-#---------------------------------#
-# Header                          #
-#---------------------------------#
 [cmdletbinding()]
-param([string[]]$Task = 'Analyze')
+param([string[]]$Task)
 $ErrorActionPreference = 'Stop'
 
 Write-Host 'Running AppVeyor Analyze script' -ForegroundColor Yellow
@@ -24,5 +9,9 @@ Write-Host "Current working directory: $pwd"
 #---------------------------------#
 # Run Psake to Analyze the code   #
 #---------------------------------#
-Invoke-psake -buildFile ".\*\SyncADContacts.build.ps1" -taskList $Task -Verbose:$VerbosePreference
-if ($psake.build_success -eq $false) {exit 1 } else { exit 0 }
+
+$SAResults = Invoke-ScriptAnalyzer -Path ".\*\SyncADContacts\" -Recurse -Verbose:$false
+    if($SAResults){
+        $SAResults | Format-Table
+        Write-Error -Message 'one or more Script Analyzer errors/warnings were found. Build cannot continue! '
+    }
