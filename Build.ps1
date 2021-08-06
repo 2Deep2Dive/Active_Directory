ced50c761faa4012dff9f3bc964c39184407c57d
Mohamed Eliwa <mohamed.eliwa@outlook.com>
2021-08-06 11:40:38 +0200

    removed the switch, added to a new build file

--- Build.ps1
+++ Build.ps1
@@ -6,28 +6,6 @@ if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Fo
 if (!(Get-Module -Name psake -ListAvailable)) { Install-Module -Name psake -MinimumVersion 4.8.0 -Force} else{update-Module -Name psake -Force}
 if (!(Get-Module -Name PSDeploy -ListAvailable)) { Install-Module -Name PSDeploy -Force -Verbose}        else{update-Module -Name PSDeploy -Force}
 if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Force}
-
-switch ($Task) {
-    'Analyze' {
-        Write-Information 'Running AppVeyor Analyze script'
-        Invoke-psake -buildFile  ".\AppVeyor\SyncADContacts.analyze.ps1" -taskList $Task -Verbose:$VerbosePreference
-
-    }
-    'Build' {
-        Invoke-psake -buildFile ".\AppVoyer\appvoyer.bild.ps1" -taskList $Task -Verbose:$VerbosePreference
-
-    }
-    'Test' {
-        Invoke-psake -buildFile ".\AppVoyer\appvoyer.test.ps1" -taskList $Task -Verbose:$VerbosePreference
-
-    }
-    'Deploy' {
-        Invoke-psake -buildFile ".\AppVoyer\appvoyer.deploy.ps1" -taskList $Task -Verbose:$VerbosePreference
-    }
-    'Clean' {
-        Invoke-psake -buildFile ".\AppVoyer\appvoyer.clean.ps1" -taskList $Task -Verbose:$VerbosePreference
-    }
-    Default {}
-}
-
+$ModuleName = (Get-Item $PWD).BaseName
+Invoke-psake -buildFile ".\$ModuleName\$ModuleName.build.ps1" -taskList $Task -Verbose:$VerbosePreference
 if ($psake.build_success -eq $false) {exit 1 } else { exit 0 }
